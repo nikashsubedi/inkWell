e70b0b413c69a52b2a92725d65b71f6672cc69da
nikash <nikash.subedi101@gmail.com>
2025-07-28 11:20:36 +0545

    postlist

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Test from '../test/Test';
 import React from 'react';
+import CardList from '../card/CardList'; // Import the CardList component
 
 const router = createBrowserRouter(
 
@@ -16,6 +17,16 @@ const router = createBrowserRouter(
       path: '/',
 
       element: <Test />,
+    },
+
+    {
+      path: '/cards',
+
+      element: <CardList />,
+    },
+    {
+      path: '/cards', // New path to display all 1000 cards
+      element: <CardList />,
     },
    
    
